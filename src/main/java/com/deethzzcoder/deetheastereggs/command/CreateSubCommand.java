c2eaa53b4df1dcbfa8c038e059dd42b2ef5adeec
Nikita Knyazev <44509571+DeethzzCoder@users.noreply.github.com>
2018-11-28 15:37:57 +0300

    Update 0.5

--- src/main/java/com/deethzzcoder/deetheastereggs/command/CreateSubCommand.java
+++ src/main/java/com/deethzzcoder/deetheastereggs/command/CreateSubCommand.java
@@ -1,6 +1,8 @@
 package com.deethzzcoder.deetheastereggs.command;
 
+import com.deethzzcoder.deetheastereggs.configuration.GeneralConfiguration;
 import com.deethzzcoder.deetheastereggs.configuration.LanguageConfiguration;
+import com.deethzzcoder.deetheastereggs.configuration.MainConfiguration;
 import com.deethzzcoder.deetheastereggs.easteregg.EasterEgg;
 import com.deethzzcoder.deetheastereggs.easteregg.EasterEggFactory;
 import com.deethzzcoder.deetheastereggs.easteregg.EasterEggResolver;
@@ -19,36 +21,42 @@ import java.util.Arrays;
 public class CreateSubCommand extends AbstractSubCommand {
 
     private final LanguageConfiguration languageConfiguration;
+    private final MainConfiguration mainConfiguration;
     private final EasterEggResolver easterEggResolver;
     private final EasterEggFactory easterEggFactory;
 
-    CreateSubCommand(LanguageConfiguration languageConfiguration, EasterEggResolver easterEggResolver, EasterEggFactory easterEggFactory) {
+    CreateSubCommand(GeneralConfiguration generalConfiguration, EasterEggResolver easterEggResolver, EasterEggFactory easterEggFactory) {
         super("create", Arrays.asList("set", "add"), false, 1, -1);
-        this.languageConfiguration = languageConfiguration;
+        this.languageConfiguration = generalConfiguration.getLanguageConfiguration();
+        this.mainConfiguration = generalConfiguration.getMainConfiguration();
         this.easterEggResolver = easterEggResolver;
         this.easterEggFactory = easterEggFactory;
     }
 
     @Override
     protected void execute(CommandSender sender, String[] args) {
+        if(mainConfiguration.getStringList("blocked-egg-names").contains(args[0])) {
+            languageConfiguration.getBuilder("create-subcommand.blocked-name").replaceEggData(args[0]).build().send(sender);
+            return;
+        }
         if(easterEggResolver.findEasterEggByName(args[0]) != null) {
             EasterEgg easterEgg = easterEggResolver.findEasterEggByName(args[0]);
-            languageConfiguration.getBuilder("create-subcommand.already-exists-name").replaceMessage("%easter-egg-name%", easterEgg.getName()).replaceMessage("%easter-egg-prize%", easterEgg.hasPrize() ? easterEgg.getPrize() : languageConfiguration.getMessage("none").getMessage()).replaceMessage("%easter-egg-world%", easterEgg.getLocation().getWorld().getName()).replaceMessage("%easter-egg-x%", String.valueOf(easterEgg.getLocation().getX())).replaceMessage("%easter-egg-y%", String.valueOf(easterEgg.getLocation().getY())).replaceMessage("%easter-egg-z%", String.valueOf(easterEgg.getLocation().getZ())).build().send(sender);
+            languageConfiguration.getBuilder("create-subcommand.already-exists-name").replaceEggData(easterEgg.getName(), easterEgg.hasPrize() ? easterEgg.getPrize() : languageConfiguration.getMessage("none").getMessage(), easterEgg.getLocation()).build().send(sender);
             return;
         }
         Location location = ((Player) sender).getTargetBlock(null, 10).getLocation();
         if(easterEggResolver.findEasterEggByLocation(location) != null) {
             EasterEgg easterEgg = easterEggResolver.findEasterEggByLocation(location);
-            languageConfiguration.getBuilder("create-subcommand.already-exists-location").replaceMessage("%easter-egg-name%", easterEgg.getName()).replaceMessage("%easter-egg-prize%", easterEgg.hasPrize() ? easterEgg.getPrize() : languageConfiguration.getMessage("none").getMessage()).replaceMessage("%easter-egg-world%", easterEgg.getLocation().getWorld().getName()).replaceMessage("%easter-egg-x%", String.valueOf(easterEgg.getLocation().getX())).replaceMessage("%easter-egg-y%", String.valueOf(easterEgg.getLocation().getY())).replaceMessage("%easter-egg-z%", String.valueOf(easterEgg.getLocation().getZ())).build().send(sender);
+            languageConfiguration.getBuilder("create-subcommand.already-exists-location").replaceEggData(easterEgg.getName(), easterEgg.hasPrize() ? easterEgg.getPrize() : languageConfiguration.getMessage("none").getMessage(), easterEgg.getLocation()).build().send(sender);
             return;
         }
         if(args.length == 1) {
             EasterEgg easterEgg = easterEggFactory.makeEasterEgg(args[0], location);
-            languageConfiguration.getBuilder("create-subcommand.successfully").replaceMessage("%easter-egg-name%", easterEgg.getName()).replaceMessage("%easter-egg-prize%", easterEgg.hasPrize() ? easterEgg.getPrize() : languageConfiguration.getMessage("none").getMessage()).replaceMessage("%easter-egg-world%", easterEgg.getLocation().getWorld().getName()).replaceMessage("%easter-egg-x%", String.valueOf(easterEgg.getLocation().getX())).replaceMessage("%easter-egg-y%", String.valueOf(easterEgg.getLocation().getY())).replaceMessage("%easter-egg-z%", String.valueOf(easterEgg.getLocation().getZ())).build().send(sender);
+            languageConfiguration.getBuilder("create-subcommand.successfully").replaceEggData(easterEgg.getName(), languageConfiguration.getMessage("none").getMessage(), easterEgg.getLocation()).build().send(sender);
             return;
         }
         EasterEgg easterEgg = easterEggFactory.makeEasterEgg(args[0], StringUtils.merge(ArrayUtils.remove(args, 0), " "), location);
-        languageConfiguration.getBuilder("create-subcommand.successfully").replaceMessage("%easter-egg-name%", easterEgg.getName()).replaceMessage("%easter-egg-prize%", easterEgg.hasPrize() ? easterEgg.getPrize() : languageConfiguration.getMessage("none").getMessage()).replaceMessage("%easter-egg-world%", easterEgg.getLocation().getWorld().getName()).replaceMessage("%easter-egg-x%", String.valueOf(easterEgg.getLocation().getX())).replaceMessage("%easter-egg-y%", String.valueOf(easterEgg.getLocation().getY())).replaceMessage("%easter-egg-z%", String.valueOf(easterEgg.getLocation().getZ())).build().send(sender);
+        languageConfiguration.getBuilder("create-subcommand.successfully").replaceEggData(easterEgg.getName(), easterEgg.getPrize(), easterEgg.getLocation()).build().send(sender);
     }
 
 }

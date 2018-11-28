f112d4094c95b2832f797329114635ac7be974d1
Nikita Knyazev <44509571+DeethzzCoder@users.noreply.github.com>
2018-11-28 15:40:10 +0300

    Update 0.5

--- src/main/java/com/deethzzcoder/deetheastereggs/command/RelocationSubCommand.java
+++ src/main/java/com/deethzzcoder/deetheastereggs/command/RelocationSubCommand.java
@@ -27,19 +27,19 @@ public class RelocationSubCommand extends AbstractSubCommand {
     @Override
     protected void execute(CommandSender sender, String[] args) {
         if(easterEggResolver.findEasterEggByName(args[0]) == null) {
-            languageConfiguration.getBuilder("relocation-subcommand.doesnt-exist").replaceMessage("%easter-egg-name%", args[0]).build().send(sender);
+            languageConfiguration.getBuilder("relocation-subcommand.doesnt-exist").replaceEggData(args[0]).build().send(sender);
             return;
         }
         Location location = ((Player) sender).getTargetBlock(null, 10).getLocation();
         if(easterEggResolver.findEasterEggByLocation(location) != null) {
             EasterEgg easterEgg = easterEggResolver.findEasterEggByLocation(location);
-            languageConfiguration.getBuilder("relocation-subcommand.already-exists-location").replaceMessage("%easter-egg-name%", easterEgg.getName()).replaceMessage("%easter-egg-prize%", easterEgg.hasPrize() ? easterEgg.getPrize() : languageConfiguration.getMessage("none").getMessage()).replaceMessage("%easter-egg-world%", easterEgg.getLocation().getWorld().getName()).replaceMessage("%easter-egg-x%", String.valueOf(easterEgg.getLocation().getX())).replaceMessage("%easter-egg-y%", String.valueOf(easterEgg.getLocation().getY())).replaceMessage("%easter-egg-z%", String.valueOf(easterEgg.getLocation().getZ())).build().send(sender);
+            languageConfiguration.getBuilder("relocation-subcommand.already-exists-location").replaceEggData(easterEgg.getName(), easterEgg.hasPrize() ? easterEgg.getPrize() : languageConfiguration.getMessage("none").getMessage(), easterEgg.getLocation());
             return;
         }
         EasterEgg easterEgg = easterEggResolver.findEasterEggByName(args[0]);
         Location oldLocation = easterEgg.getLocation();
         easterEgg.setLocation(location);
-        languageConfiguration.getBuilder("relocation-subcommand.successfully").replaceMessage("%easter-egg-name%", easterEgg.getName()).replaceMessage("%easter-egg-prize%", easterEgg.hasPrize() ? easterEgg.getPrize() : languageConfiguration.getMessage("none").getMessage()).replaceMessage("%easter-egg-old-world%", oldLocation.getWorld().getName()).replaceMessage("%easter-egg-old-x%", String.valueOf(oldLocation.getX())).replaceMessage("%easter-egg-old-y%", String.valueOf(oldLocation.getY())).replaceMessage("%easter-egg-old-z%", String.valueOf(oldLocation.getZ())).replaceMessage("%easter-egg-world%", easterEgg.getLocation().getWorld().getName()).replaceMessage("%easter-egg-x%", String.valueOf(easterEgg.getLocation().getX())).replaceMessage("%easter-egg-y%", String.valueOf(easterEgg.getLocation().getY())).replaceMessage("%easter-egg-z%", String.valueOf(easterEgg.getLocation().getZ())).build().send(sender);
+        languageConfiguration.getBuilder("relocation-subcommand.successfully").replaceEggData(easterEgg.getName(), easterEgg.hasPrize() ? easterEgg.getPrize() : languageConfiguration.getMessage("none").getMessage(), easterEgg.getLocation()).replaceMessage("%easter-egg-old-world%", oldLocation.getWorld().getName()).replaceMessage("%easter-egg-old-x%", oldLocation.getX()).replaceMessage("%easter-egg-old-y%", oldLocation.getY()).replaceMessage("%easter-egg-old-z%", oldLocation.getZ()).build().send(sender);
     }
 
 }

1fbbcfdeecef8918e6f5775206e25ddf89969bc2
Nikita Knyazev <44509571+DeethzzCoder@users.noreply.github.com>
2018-11-28 15:41:55 +0300

    Update 0.5

--- src/main/java/com/deethzzcoder/deetheastereggs/utility/ExceptionHandler.java
+++ src/main/java/com/deethzzcoder/deetheastereggs/utility/ExceptionHandler.java
@@ -3,20 +3,25 @@ package com.deethzzcoder.deetheastereggs.utility;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 
+import java.util.logging.Logger;
+
 /**
  * Copyright © Nikita (DeethzzCoder) Knyazev [vk.com/deethzzcoder/]
  */
 
 public final class ExceptionHandler {
 
+    private static final Logger logger = Logger.getLogger("ExceptionHandler");
+
     private ExceptionHandler() {
 
     }
 
     public static void handleException(Exception exception) {
-        LoggerUtils.warn("&cException was detected!", "&cPlease, send this exception to developer! [www.spigotmc.org/members/deethzzcoder.611082/]");
+        logger.warning(ColorUtils.color("&cException was detected!"));
+        logger.warning(ColorUtils.color("&cPlease, send this exception to developer! [www.spigotmc.org/members/deethzzcoder.611082/]"));
         exception.printStackTrace();
-        LoggerUtils.warn("&cThe plugin DeethEasterEggs will disabled!");
+        logger.warning(ColorUtils.color("&cThe plugin DeethEasterEggs will disabled!"));
         PluginManager pluginManager = Bukkit.getPluginManager();
         pluginManager.disablePlugin(pluginManager.getPlugin("DeethEasterEggs"));
     }
